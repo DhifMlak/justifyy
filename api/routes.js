28e923cf1d68a268fddd214ad723582b393fbd40
DhifMlak <Dhifmlak@gmail.com>
2019-06-04 17:58:42 +0100

    added justification

--- api/routes.js
+++ api/routes.js
@@ -1,10 +1,40 @@
 const router = require('express').Router();
 const jwt = require('jsonwebtoken');
 
-router.post('/justify',(req,res)=>{
-    res.send({ message: 'Justified text here....'})
-  })
-  router.post('/token',(req,res)=>{
-    res.send({ token: jwt.sign( req.body.email , ' secret_pass ') })
-  })
-  module.exports = router;
+
+
+
+router.post('/justify', (req, res) => {
+    res.send(justify(req.body, 80))
+})
+
+router.post('/token', (req, res) => {
+    res.send({ token: jwt.sign(req.body.email, ' secret_pass ') })
+})
+
+const justify = (text, len) => {
+    const re = RegExp("(?:\\s|^)(.{1," + len + "})(?=\\s|$)", "g");
+    lines = text.split(re);
+
+
+    lines = lines.filter((line) => line !== "");
+    let i = -1;
+    while (++i < lines.length-1) {
+        let spaceIndex = lines[i].indexOf(' ');
+      
+        while (lines[i].length < len) {
+           lines[i] = lines[i].insertAt( spaceIndex," ");
+            spaceIndex = lines[i].indexOf(' ', spaceIndex + 2);
+        }
+
+    }
+     return lines.join('\n');;
+}
+String.prototype.insertAt = function (index,string) {
+    if (index > 0)
+    return this.substring(0, index) + string + this.substring(index, this.length);
+
+  return string + this;
+};
+
+module.exports = router;
--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyparser = require('body-parser');
 const app = express();
-app.use(bodyparser.json());
+app.use(bodyparser.text());
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
